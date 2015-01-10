6c21ae91e67eaf749aba17569b3c5b0c7f7dc975
Boris Filipov <obiwonn@gmail.com>
2015-01-10 03:53:38 +0200

    stable version working as proof-of-concept

--- README.md
+++ README.md
@@ -11,8 +11,13 @@ Organelle for generating documentation by runtime reflection of express mounted 
       "dna": {
         "organic-api-routes": "@processes.index.plasma.organic-api-routes"
       },
-      "mount": "/docs/api",
-      "reactOn": ["ExpressServer"]
+      "reactOn": ["ExpressServer"],
+      "mountOn": "/api-docs",
+      "docsMetadata": {
+        "source": "docs/api"
+      },
+      "log": true,
+      "liveTemplateReload": true
     }
 
 ## `dna` property
@@ -20,3 +25,26 @@ Organelle for generating documentation by runtime reflection of express mounted 
 Its value is used to build organelles responsible for [express routes mounting](https://github.com/outbounder/organic-express-routes).
 
 All organelles are expected to `reactOn` and to `emitReady`. Additionally `log: false` is applied to their corresponding dna before triggering their build.
+
+## `docsMetadata` property
+
+Indicates additional metadata information by default stored in markdown files where api routes are matched as follows
+
+    # markdown file title
+
+    ## GET /api/resource
+
+    this text will be merged as description for `GET /api/resource` action,
+    other supported methods are "POST", "PUT", "DELETE" and "ALL"
+
+    ## PUT /api/resource/:url_param ? :query_params
+
+    example markdown metadata description for action with url and query params.
+
+    ### url params
+
+    * :url_param
+
+    ### query params
+
+    * :query_params
--- index.js
+++ index.js
@@ -1,111 +1,100 @@
 var Nucleus = require("organic-nucleus")
+var Plasma = require("organic-plasma")
 var clone = require("clone")
-var markedejs = require("markedejs")
-var fs = require("fs")
-var path = require("path")
-var esprima = require("esprima")
 var async = require("async")
-var _ = require("underscore")
 
-var addSpaces = function(count) {
-  var result = ""
-  for(var i = 0; i<count; i++)
-    result += " "
+var ExpressDoc = require("./lib/express-app-to-documentation")
+var DocGenerator = require("./lib/documentation-generator")
+
+var acquireOrganelleDNA = function(dna){
+  var result = clone(dna)
+  result.reactOn = "document"
+  result.emitReady = "documentDone"
+  result.log = false
   return result
 }
 
-var ExpressMock = function(){
-  var self = this
-  var methods = ["get", "post", "put", "delete", "all"]
+var generateDocs = function(dna, done){
+  var results = []
 
-  this.documentation = {
-    actions: [/*{
-      url: "/api/bla/:id",
-      method: "POST",
-      comments: "# Markdown"
-    }*/]
-  }
-
-  this.use = function(){
-    console.warn("used `use` but not implemented", arguments)
-  }
-  methods.forEach(function(method){
-    self[method] = function(url, handlers){
-      if(!Array.isArray(handlers))
-        handlers = [handlers]
-      var action = {
-        url: url,
-        method: method,
-        comments: ""
-      }
-      handlers.forEach(function(handler, index){
-        var handler_source = "module.exports = "+handler.toString()
-        var ast = esprima.parse(handler_source, {comment: true})
-        var comments = ast.comments.map(function(c){
-          return c.value
-        }).join("\n")
-        if(comments.indexOf("# action") == -1) return
-        comments = comments.replace("# action", "")
-        action.comments += comments
-      })
-      
-      if(action.comments)
-        self.documentation.actions.push(action)
-    }
-  })
-}
-var generateStaticHtml = function(doc, done){
-  
-  doc.actions = _.sortBy(doc.actions, function(a){
-    return a.url+a.method
-  })
-
-  async.map(doc.actions, function(action, next){
-    markedejs.render(action.comments, {doc: doc, action: action}, function(err, html){
-      if(err) return next(err)
-      action.comments = html
-      next()
+  // construct and execute documenting tasks in async series
+  var tasks = []
+  for(var key in dna.dna){
+    tasks.push({
+      dna: acquireOrganelleDNA(dna.dna[key])
     })
+  }
+  async.eachSeries(tasks, function(task, nextTask){
+    // build task's dna as Organelle using Express based Chemical to reac on
+    var subplasma = new Plasma()
+    var nucleus = new Nucleus(subplasma, {})
+    nucleus.build(task.dna)
+
+    // fake Express Server
+    var fakeExpress = new ExpressDoc(dna)
+
+    // listen once Organelle is ready
+    subplasma.on("documentDone", function(){
+      results.push(fakeExpress.generateDocumentation())
+      nextTask()
+    })
+
+    // trigger Organelle's responsibility
+    subplasma.emit({
+      type: "document",
+      data: fakeExpress
+    })  
   }, function(err){
     if(err) return done(err)
-    fs.readFile(path.join(__dirname,"template.html"), function(err, templateData){
-      if(err) return done(err)
-      var filepath = path.join(__dirname, "output.html")
-      markedejs.render(templateData.toString(), doc, function(err, html){
-        if(err) return done(err)
-        fs.writeFile(filepath, html, function(err){
-          if(err) return done(err)
-          done(null, filepath)
-        })  
-      })
-    })
+    done(null, results)
   })
-  
 }
-module.exports = function(plasma, dna) {
-  var documentExpressDNA = { plasma: clone(dna.dna) }
-  for(var key in documentExpressDNA.plasma) {
-    documentExpressDNA.plasma[key].reactOn = "document"
-    documentExpressDNA.plasma[key].emitReady = "documentDone"
-    documentExpressDNA.plasma[key].log = false
+
+var generateHtml = function(docs, dna, next){
+  if(docs.length != 1) {
+    return next("not implemented yet!")
   }
-  var nucleus = new Nucleus(plasma, documentExpressDNA)
-  nucleus.build("plasma")
-  plasma.on(dna.reactOn, function(c){
-    var app = c.data || c[0].data
-    var fakeExpressMock = new ExpressMock()
-    plasma.on("documentDone", function(){
-      generateStaticHtml(fakeExpressMock.documentation, function(err, file){
-        if(err) console.error(err)
-        console.info("documentation updated and accessible at -> ", dna.mount)
-        app.get(dna.mount, function(req, res, next){
-          res.sendfile(file)
+  var g = new DocGenerator({
+    templatePath: dna.templatePath,
+    metadata: dna.docsMetadata
+  })
+  g.generateHtml(docs[0], next)
+}
+
+module.exports = function(plasma, dna) {
+  if(dna.reactOn)
+    plasma.on(dna.reactOn, function(c){
+      var app = c.data || c[0].data
+      var response = { html: "", generatedDocs: null}
+      app.get(dna.mountOn, function(req, res, next){
+        if(!dna.liveTemplateReload)
+          res.status(200).send(response.html).end()
+        else
+          generateHtml(response.generatedDocs, dna, function(err, html){
+            if(err) return next(err)
+            res.status(200).send(html).end()
+          })
+      })
+      generateDocs(dna, function(err, docs){
+        if(err) return console.error(err)
+        response.generatedDocs = docs
+        generateHtml(docs, dna, function(err, html){
+          if(err) return console.error(err)
+          if(!html) return console.error("html is not found!!!!", err, html)
+          response.html = html
+          if(dna.emitReady)
+            plasma.emit({type: dna.emitReady, data: { docs: docs, html: html } })
+          if(dna.log)
+            console.info("--------------------------- generated api docs and mounted at", dna.mountOn)
         })
       })
     })
-    plasma.emit({
-      type: "document",
-      data: fakeExpressMock
+  else
+    generateDocs(dna, function(err, docs){
+      if(err) return console.error(err)
+      if(dna.emitReady)
+        plasma.emit({type: dna.emitReady, data: {docs: docs} })
+      if(dna.log)
+        console.info("--------------------------- generated api docs struct done")
     })
-  })
 }
